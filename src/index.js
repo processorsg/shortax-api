9163d9d2e6a5a25deb850ca6a81e3736e056f950
processorsg <processorsg@gmail.com>
2020-09-18 19:47:47 +0530

    cors issue fix

--- src/index.js
+++ src/index.js
@@ -1,56 +1,65 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const session = require('express-session')
-const morgan = require('morgan')
-const helmet = require('helmet')
-const MongoStore = require('connect-mongo')(session)
-const connectDB = require('./db/mongo')
-const userRouter = require('./routers/userRouter')
-const urlRouter = require('./routers/urlRouter')
-const URL = require('./models/URL')
+const express = require("express");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
+const MongoStore = require("connect-mongo")(session);
+const connectDB = require("./db/mongo");
+const userRouter = require("./routers/userRouter");
+const urlRouter = require("./routers/urlRouter");
+const URL = require("./models/URL");
 
-const app = express()
+const app = express();
 
 //Loading confing file in development mode
-if (process.env.NODE_ENV === 'development') {
-    require('dotenv').config()
+if (process.env.NODE_ENV === "development") {
+  require("dotenv").config();
 }
 
 //connecting DB
-connectDB()
+connectDB();
 
 //Express Middlewares
-app.use(express.json())
+app.use(express.json());
 
 //User sessions
-app.set('trust proxy', 1) // trust first proxy
-app.use(session({
+app.set("trust proxy", 1); // trust first proxy
+app.use(
+  session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-    store: new MongoStore({ mongooseConnection: mongoose.connection })
-}))
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  })
+);
 
 //logger and helmet for security
-app.use(morgan('dev'))
-app.use(helmet())
+app.use(cors());
+app.use(morgan("dev"));
+app.use(helmet());
 
 //userroutes and urlroutes
-app.use(userRouter)
-app.use(urlRouter)
+app.use(userRouter);
+app.use(urlRouter);
 
 //redirect url
-app.get('/:id', async (req, res) => {
-    try {
-        const url = await URL.findOne({ slug: req.params.id })
-        if (!url) {
-            throw new Error('URL not found...!')
-        }
-
-        res.redirect(url.longUrl)
-    } catch (error) {
-        res.status(404).json({ message: error.message })
+app.get("/:id", async (req, res) => {
+  try {
+    const url = await URL.findOne({ slug: req.params.id });
+    if (!url) {
+      throw new Error("URL not found...!");
     }
-})
 
-app.listen(process.env.PORT, console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`))
+    res.redirect(url.longUrl);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+});
+
+app.listen(
+  process.env.PORT,
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`
+  )
+);
